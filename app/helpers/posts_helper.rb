2719a8bc54955a91d8569b8c58945ae81be6881d
Samo <samo.zeleznik5@gmail.com>
2020-11-24 12:07:58 +0100

    fix helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,17 +2,15 @@ module PostsHelper
   def display_index_post_links(post)
     capture do
       concat link_to('Preberi več', post, class: 'stretched-link')
-      if current_user == post.user
-        concat link_to('Uredi', edit_post_path(post), class: 'stretched-link')
-        concat link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'stretched-link')
-      end
     end
   end
 
   def display_show_post_links(post)
     capture do
       if current_user == post.user
-        concat link_to 'Edit', edit_post_path(post)
+        concat link_to 'Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }
+        concat ' | '
+        concat link_to 'Uredi', edit_post_path(post)
         concat ' | '
       end
       concat link_to 'Back', posts_path
